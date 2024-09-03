3474813afc6ba45664d7728c40fac87525b7f9c9
Tom Tan <thomas_kf_tan@mom.gov.sg>
2024-09-03 16:41:40 +0800

    works in VSCODE

---
+++ src/main/java/com/tomtan/spring_test_vs_code/SpringTestVsCodeApplication.java
@@ -0,0 +1,13 @@
+package com.tomtan.spring_test_vs_code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestVsCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestVsCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomtan/spring_test_vs_code/SpringTestVsCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomtan.spring_test_vs_code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestVsCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
