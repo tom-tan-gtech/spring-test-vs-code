88ca5b76d847df119be9f090c46810ddb46e1fc0
Tom Tan <thomas_kf_tan@mom.gov.sg>
2024-09-03 17:28:33 +0800

    add code from the courses eclipse project

--- src/main/java/com/tomtan/spring_test_vs_code/SpringTestVsCodeApplication.java
+++ src/main/java/com/tomtan/spring_test_vs_code/SpringTestVsCodeApplication.java
@@ -1,13 +1,16 @@
 package com.tomtan.spring_test_vs_code;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class SpringTestVsCodeApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(SpringTestVsCodeApplication.class, args);
-	}
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 
+		Student s = (Student) context.getBean("student_bean");
+		s.sayHello();
+
+	}
 }
---
+++ src/main/java/com/tomtan/spring_test_vs_code/Student.java
@@ -0,0 +1,9 @@
+package com.tomtan.spring_test_vs_code;
+
+public class Student {
+
+	public void sayHello() {
+		System.out.println("Hello world from Spring Framework ...");
+	}
+
+}
