ee347b8228dd366550f93fcaf30acb14699bd6a8
TT <thomastan@Thomass-MacBook-Air.local>
2024-09-16 18:00:11 +0800

    hybrid solution that used annotation to inject address bean

--- src/main/java/com/tomtan/spring_test_vs_code/Student.java
+++ src/main/java/com/tomtan/spring_test_vs_code/Student.java
@@ -1,6 +1,10 @@
 package com.tomtan.spring_test_vs_code;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Student {
+	
+	@Autowired
 	private Address address;
 
 	public Student(Address address) {
