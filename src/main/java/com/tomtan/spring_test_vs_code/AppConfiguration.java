edb7eed44f611e9696ba8027e29fc12ee3eb3f60
TT <thomastan@Thomass-MacBook-Air.local>
2024-09-17 11:13:30 +0800

    added @scope annotation

--- src/main/java/com/tomtan/spring_test_vs_code/AppConfiguration.java
+++ src/main/java/com/tomtan/spring_test_vs_code/AppConfiguration.java
@@ -2,11 +2,13 @@ package com.tomtan.spring_test_vs_code;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfiguration {
-    
+
     @Bean(name="student_bean")
+    @Scope(value="singleton")
     public Student getStudent() {
         return new Student();
     }
